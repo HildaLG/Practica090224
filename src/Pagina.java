54e73fcdca00241e807ba286c028fbf636ecb6a6
20012021 <20012021@DESKTOP-GJHI83P>
2024-02-11 12:15:17 -0600

    Clase Usuario Video

---
+++ src/Pagina.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 20012021
+ */
+public class Pagina {
+     public static void main(String[] args) {
+        Usuario alum1= new Usuario ("Hilda");
+        Video video1=new Video ("Mi video",60,"https.//blablabla");
+        alum1.subirVideo(video1);
+        alum1.mostrarinfoUsuario();
+    }
+    
+}
---
+++ src/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 20012021
+ */
+public class Usuario {
+    
+    String Nombre;
+    Video videoSubido;
+    
+    public Usuario (String Nombre){
+        this.Nombre=Nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Video getVideoSubido() {
+        return videoSubido;
+    }
+
+    public void setVideoSubido(Video videoSubido) {
+        this.videoSubido = videoSubido;
+    }
+    /*METODOS*/
+    public void subirVideo(Video videoSubido){
+       this.videoSubido=videoSubido;             
+    }
+    public void mostrarinfoUsuario(){
+       System.out.println("Usuario: "+ Nombre);
+       if (videoSubido != null){
+       System.out.println("Ultimo video subido: "+videoSubido.getTitulo());
+       }else{
+       System.out.println("Aun no ha subido ningun video!");
+       }
+    }
+    
+    
+    
+}
